daa125e4c36e794bb54b347cf3dd7f2fae09114d
Abner-Guimaraes <Abner.guimaraes@sou.unifal-mg.edu.br>
2025-11-11 18:25:56 -0300

    feat:Starting Spring Boot

---
+++ BlueVelvet/BlueVelvet/src/main/java/com/musicstore/BlueVelvet/BlueVelvetApplication.java
@@ -0,0 +1,13 @@
+package com.musicstore.BlueVelvet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueVelvetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueVelvetApplication.class, args);
+	}
+
+}
